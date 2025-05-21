541eec676e6884c0ffc3e1ae6c6cd985af0642e0
shannonsumner <108552773+shannonsumner@users.noreply.github.com>
2025-05-21 12:32:25 -0400

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,6 +1,6 @@
 const decorateCarousel = async (block) => {
   const carouselItems = [...block.children];
-  /* while (block.firstChild) {
+  while (block.firstChild) {
     block.removeChild(block.firstChild);
   }
   block.append(addNavigationButton('&#8249;', 'previous'));
@@ -32,7 +32,7 @@ const decorateCarousel = async (block) => {
     slide.append(carouselCaption);
     carouselSlideContainer.append(slide);
   });
-  block.append(carouselSlideContainer); */
+  block.append(carouselSlideContainer);
 };
 
 const addNavigationButton = (text, direction) => {
