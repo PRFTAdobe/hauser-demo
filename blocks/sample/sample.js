0bf59e93a20e7efd337748d6fa6fee0ad0e825a2
shannonsumner <108552773+shannonsumner@users.noreply.github.com>
2025-05-23 10:26:11 -0400

    Update sample.js

--- blocks/sample/sample.js
+++ blocks/sample/sample.js
@@ -15,43 +15,74 @@ const decorateSample = (block) => {
     richText,
     select,
   ] = blockElements;
+  if (simpleText) {
+    block.insertAdjacentHTML(
+      'beforeend',
+      `<p><strong>Simple Text: </strong>${simpleText}</p>`,
+    );
+  }
   block.insertAdjacentHTML(
     'beforeend',
-    `<p><strong>Simple Text: </strong>${simpleText}</p>`,
+    `<p><strong>Boolean: </strong>${boolean === true}</p>`,
   );
-  block.insertAdjacentHTML(
-    'beforeend',
-    `<p><strong>Boolean: </strong>${boolean}</p>`,
-  );
-  block.insertAdjacentHTML(
-    'beforeend',
-    `<p><strong>Checkbox Group: </strong>${checkboxGroup}</p>`,
-  );
-  block.insertAdjacentHTML(
-    'beforeend',
-    `<p><strong>Date and Time: </strong>${dateAndTime.toLocaleDateString('en', {
-      day: 'numeric',
-      month: 'short',
-      timeZone: 'UTC',
-      year: 'numeric',
-    })}</p>`,
-  );
-  block.insertAdjacentHTML(
-    'beforeend',
-    `<p><strong>Number: </strong>${number}</p>`,
-  );
-  block.insertAdjacentHTML(
-    'beforeend',
-    `<p><strong>Radio Group: </strong>${radioGroup}</p>`,
-  );
-  block.insertAdjacentHTML(
-    'beforeend',
-    `<div><p><strong>Rich Text: </strong></p>${richText.outerHTML}</div>`,
-  );
-  block.insertAdjacentHTML(
-    'beforeend',
-    `<p><strong>Select: </strong>${select}</p>`,
-  );
+
+  if (checkboxGroup) {
+    block.insertAdjacentHTML(
+      'beforeend',
+      `<p><strong>Checkbox Group: </strong>${checkboxGroup}</p>`,
+    );
+  }
+
+  // eslint-disable-next-line no-restricted-globals
+  if (dateAndTime && !isNaN(Date.parse(dateAndTime))) {
+    block.insertAdjacentHTML(
+      'beforeend',
+      `<p><strong>Date and Time: </strong>${dateAndTime.toLocaleDateString(
+        'en',
+        {
+          day: 'numeric',
+          month: 'short',
+          timeZone: 'UTC',
+          year: 'numeric',
+        },
+      )}</p>`,
+    );
+  }
+
+  if (number) {
+    block.insertAdjacentHTML(
+      'beforeend',
+      `<p><strong>Number: </strong>${number}</p>`,
+    );
+  }
+
+  if (radioGroup) {
+    block.insertAdjacentHTML(
+      'beforeend',
+      `<p><strong>Radio Group: </strong>${radioGroup}</p>`,
+    );
+  }
+
+  if (richText) {
+    if (typeof richText.outerHTML !== 'undefined') {
+      block.insertAdjacentHTML(
+        'beforeend',
+        `<div><p><strong>Rich Text: </strong></p>${richText.outerHTML}</div>`,
+      );
+    } else {
+      block.insertAdjacentHTML(
+        'beforeend',
+        `<p><strong>Rich Text: </strong>${richText}</p>`,
+      );
+    }
+  }
+
+  if (select) {
+    block.insertAdjacentHTML(
+      'beforeend',
+      `<p><strong>Select: </strong>${select}</p>`,
+    );
+  }
 };
 
 export default decorateSample;
